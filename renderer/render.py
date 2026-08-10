38170bf656e12cc58ddfe95f8531739e9a4341b8
aadeshjaipal0-glitch <aadeshjaipal0@gmail.com>
2026-08-10 11:01:47 +0500

    Update render.py

--- renderer/render.py
+++ renderer/render.py
@@ -1,16 +1,13 @@
 import subprocess
 from pathlib import Path
 
-OUTPUT = Path("final_reel.mp4")
+OUTPUT = Path("test_render.mp4")
 
 cmd = [
     "ffmpeg",
     "-y",
-
     "-f", "lavfi",
-    "-i",
-    "color=c=black:s=1080x1920:r=30:d=5",
-
+    "-i", "color=c=black:s=1080x1920:r=30:d=5",
     "-vf",
     (
         "drawtext="
@@ -22,11 +19,9 @@ cmd = [
         "y=(h-text_h)/2:"
         "enable='between(t,0,5)'"
     ),
-
     "-c:v", "libx264",
     "-preset", "veryfast",
     "-pix_fmt", "yuv420p",
-
     str(OUTPUT)
 ]
 
